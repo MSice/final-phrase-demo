461595e751739a9e89e5c625967088052127d6e7
huangwensong <huangwensong@58.com>
2024-03-24 16:37:42 +0800

    feat: header优化

--- src/components/header/useHeader.ts
+++ src/components/header/useHeader.ts
@@ -2,23 +2,20 @@
  * @Author: huangwensong
  * @Date: 2024-03-24 14:40:18
  * @LastEditors: huangwensong
- * @LastEditTime: 2024-03-24 16:29:59
+ * @LastEditTime: 2024-03-24 16:36:02
  * @FilePath: /final-phrase-demo/src/components/Header/useHeader.ts
  * @Description:
  */
 
 import { reactive, toRefs, onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
 
 const headerHideList: any = ['/', '/home'];
 
 const useHeader = () => {
-    const route = useRoute();
     const showHeader = ref(true);
     onMounted(() => {
-        console.log(route);
         headerHideList.map((item: string) => {
-            item === route.path && (showHeader.value = false);
+            item === location.pathname && (showHeader.value = false);
         });
 
         console.log(showHeader.value);
